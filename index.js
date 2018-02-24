823b3dc977efb66ad51b0bda5133aaf21b7eec08
Maryann Jordan <majch@DESKTOP-KQJEU0U.(none)>
2018-02-23 22:08:23 -0600

    improvemt comment

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
   * Randomly selects a word and uses the `Word` constructor to store it
   * Prompts the user for each guess and keeps track of the user's remaining guesses
   Author: Maryann Jordan
+  improvements: add data verification
 */
 var inquirer = require("inquirer");
 var Letter = require("./letter.js")
